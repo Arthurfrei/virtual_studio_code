14763904be008866f2a13b76e1239ba5c2052f14
Arthurfrei <116167466+Arthurfrei@users.noreply.github.com>
2023-05-23 11:36:18 -0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+</head>
+<body>
+<form id = "login" action = "logado.php" method = "POST">
+login: <input type = "text" name = "aluno"><br>
+senha: <input type = "password" name = "FPB"><br>
+<input type = "submit" name = "entrar" value = "entrar">
+
+    </form>
+    </body>       
+    </html> 
