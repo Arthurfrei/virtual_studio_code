eccbee042e623c32cbc6381d43da20cf71be088f
Arthurfrei <116167466+Arthurfrei@users.noreply.github.com>
2023-06-01 09:53:19 -0300

    Create cadastro.php

---
+++ cadastro.php
@@ -0,0 +1,12 @@
+<html>
+
+<body>
+    <h1>cadastro</h1>
+    <form id = "cadastro" action = "cadastrar.php" method = "post">
+    nome: <input type = "text" name = "nome" required><br>
+    login: <input type ="text" name = "login" required><br>
+    senha: <input type = "password" name = "senha" required><br><br>
+    <input type = "submit" name = "cadastrar" value = "cadastrtar">
+
+</body>
+</html>
