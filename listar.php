547fcc3397dc0c3c93b11f4903cbc88fbf475de3
Arthurfrei <116167466+Arthurfrei@users.noreply.github.com>
2023-06-06 11:47:26 -0300

    Create listar.php

---
+++ listar.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+ <meta....>
+ <meta....>
+ <link rel = "stylesheet" type = "text/css" href = "estilo.css">
+</head>
+</body>
+
+<?php echo $_SESSION['nome'];?><br>
+<center>
+    <h1>lista de usuarios</h1>
+
+    <a href = "usuarios.sql">usuarios</a><br>
+
+
+
+<center>
+</body>
+</html>
