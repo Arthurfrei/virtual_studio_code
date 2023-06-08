248aeb661a92fe9b54967e5331490867b058d2d1
Arthurfrei <116167466+Arthurfrei@users.noreply.github.com>
2023-06-08 19:19:47 -0300

    Update senha.php

--- senha.php
+++ senha.php
@@ -1,13 +1,26 @@
-<html>
-<head>
- <meta....>
- <meta....>
- <link rel = "stylesheet" type = "text/css" href = "estilo.css">
-</head>
-<body>
-    <center>
-    <h1>senha alterada com sucesso</h1>
-    <a href = "index.php">voltar</a>
-    </center>
-</body>
-</html>
+<?php
+session_start();
+include_once('conexao.php');
+
+$SendEditCont = filter_input(INPUT_POST, 'SendEditcont', FILTER_SANITIZE_STRING);
+if($SendEditCont){
+
+$login = filter_input(INPUT_POST, 'login', FILTER_SANITIZE_STRING);
+$senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
+
+$result_msg_cont = "UPDATE login SET login=:login,senha=:senha";
+
+$update_msg_cont = $conexao->prepare($result_msg_cont);
+$update_msg_cont->bindParam(':login', $login);
+$update_msg_cont->bindParam(': senha', $senha);
+
+
+if ($update_msg_cont->execute()){
+    $_SESSION['msg'] = "<p>Senha alterada com sucesso</p>";
+    hearder("Location: alterar.php");
+}else{
+    $_SESSION['msg'] = "<p>Senha não foi alterada com sucesso</p>";
+    hearder("Location: alterar.php");
+}
+}
+?>
