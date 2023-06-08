5284cd8ca6691f404ffa2a123b1d0bc5945f6787
Arthurfrei <116167466+Arthurfrei@users.noreply.github.com>
2023-06-08 17:34:15 -0300

    Create senha.php

---
+++ senha.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+ <meta....>
+ <meta....>
+ <link rel = "stylesheet" type = "text/css" href = "estilo.css">
+</head>
+<body>
+    <center>
+    <h1>senha alterada com sucesso</h1>
+    <a href = "index.php">voltar</a>
+    </center>
+</body>
+</html>
